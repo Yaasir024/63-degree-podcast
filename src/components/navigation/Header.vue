c505ca5eac6c5935c758c0ab95d030baff9ffb2a
Yaasir024 <yope4ever@gmail.com>
2023-07-10 15:22:58 +0100

    /navigation

--- src/components/navigation/Header.vue
+++ src/components/navigation/Header.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+import MobileNav from '@/components/navigation/MobileNav.vue';
+
+import { useClickOutside } from "@/composables/useClickOutside";
+
+const mobileNav = ref(null)
+const showMobileNav = ref(false)
+
+useClickOutside(mobileNav, () => {
+    showMobileNav.value = false;
+});
+</script>
+
 <template>
     <header class="bg-63-dark-green py-[16px] px-[28px] xs:px-[42px] md:px-[64px] xl:px-[96px]  2xl:px-[112px] ">
         <div class="flex items-center justify-between">
@@ -26,14 +40,52 @@
             </div>
             <div class="">
                 <RouterLink to="/episodes" class="hidden md:block">
-                    <button class="bg-63-light-green text-63-dark-green rounded-lg px-[20px] py-[12px] text-[18px] leading-[28px] font-semibold">Visit Episodes</button>
+                    <button
+                        class="bg-63-light-green text-63-dark-green rounded-lg px-[20px] py-[12px] text-[18px] leading-[28px] font-semibold">Visit
+                        Episodes</button>
                 </RouterLink>
-                <div class="relative block md:hidden">
-                    <button class="text-white">
-                        <svg height="24" width="24" fill="currentColor" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/></svg>
+                <div class="relative block md:hidden" ref="mobileNav">
+                    <button class="text-white" @click="showMobileNav = !showMobileNav">
+                        <svg height="24" width="24" fill="currentColor" clip-rule="evenodd" fill-rule="evenodd"
+                            stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z"
+                                fill-rule="nonzero" />
+                        </svg>
                     </button>
+                    <transition name="nav">
+                        <MobileNav v-if="showMobileNav" @close="showMobileNav = false" />
+                    </transition>
                 </div>
             </div>
         </div>
+        <div class="overlay h-screen fixed top-0 left-0 bottom-0 right-0 bg-black/40 z-[80]" v-if="showMobileNav">
+        </div>
     </header>
 </template>
+
+<style scoped>
+/* Nav Animation */
+.nav-enter-active,
+.nav-leave-active {
+    transition: all 0.4s ease;
+}
+
+.nav-enter-from,
+.nav-leave-to {
+    opacity: 0;
+    transform: translateX(100%);
+}
+
+/* Menu Animation */
+.menu-enter-active,
+.menu-leave-active {
+    transition: transform 0.3s ease;
+    transform-origin: top left;
+}
+
+.menu-enter-from,
+.menu-leave-to {
+    transform: scale(0);
+}</style>
---
+++ src/components/navigation/MobileNav.vue
@@ -0,0 +1,36 @@
+<script setup>
+const emit = defineEmits(["close"])
+
+const closeOnRoute = () => {
+    emit('close')
+}
+</script>
+
+<template>
+    <div class="w-[80vh] sm:w-[50vh] h-screen fixed top-0 bottom-0 right-0 bg-63-dark-green z-[100]">
+        <div class="flex items-center justify-end py-3 px-4">
+            <button class="text-white" @click="$emit('close')">
+                <svg width="24" height="24" fill="currentColor" clip-rule="evenodd" fill-rule="evenodd"
+                    stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
+                </svg>
+            </button>
+        </div>
+        <ul class="flex flex-col text-white">
+            <li class=" text-[20px] leading-[24px] pl-8 py-4">
+                <RouterLink to="/" class="font-medium" @click="closeOnRoute">Home</RouterLink>
+            </li>
+            <li class=" text-[20px] leading-[24px] pl-8 py-4">
+                <RouterLink to="/episodes" class="font-medium" @click="closeOnRoute">
+                    Episodes
+                </RouterLink>
+            </li>
+            <li class=" text-[20px] leading-[24px] pl-8 py-4">
+                <RouterLink to="/" class="font-medium" @click="closeOnRoute">Blog</RouterLink>
+            </li>
+            <li class=" text-[20px] leading-[24px] pl-8 py-4">
+                <RouterLink to="/contact" class="font-medium" @click="closeOnRoute">Contact</RouterLink>
+            </li>
+    </ul>
+</div></template>
