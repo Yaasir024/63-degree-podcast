93e63ab6bf7abd390004cc293cc0bf98e1a49539
Yaasir024 <yope4ever@gmail.com>
2023-07-10 14:41:05 +0100

    /abput page responsiveness fix

--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -1,25 +1,34 @@
 <template>
-    <section class="bg-63-dark-green px-[42px] md:px-[64px] xl:px-[112px] py-[96px]">
+    <section class="bg-63-dark-green px-[28px] xs:px-[42px] md:px-[64px] xl:px-[112px] py-[96px]">
         <div class="flex w-full flex-wrap">
-            <div class="flex-full lg:flex-50% pr-[32px] xl:pr-[64px]">
+            <div class="flex-full lg:flex-50% lg:pr-[32px] xl:pr-[64px]">
                 <img src="@/assets/images/home/about.png" alt="" class="">
             </div>
             <div class="flex-full lg:flex-50%  py-[40px]">
-                <h3 class="text-[32px] md:text-[48px] leading-[48px] md:leading-[60px] font-semibold text-[#6AE6D4]">
+                <h3 class="text-[32px] md:text-[48px] leading-[38px] md:leading-[60px] font-semibold text-[#6AE6D4]">
                     Meet Yvonne Marcus
                 </h3>
 
-                <div class="mt-[24px] text-[18px] md:text-[24px] leading-[30px] lg:max-w-[480px] text-[#F2F2F2]">
-                    She is an art director and animator with a decade of experience in editorial, documentary, and
-                    advertising.
-                    <br>
-                    She’s worked for big names like The Atlantic, Adobe, Vox, and Netflix.
-                    <br>
-                    When she’s not making rad creative work, Yvonne loves to speak and educate on animation and motion
-                    design, and has given talks and run student workshops across the U.S. and internationally.
+                <div
+                    class="mt-[24px] text-base sm:text-[18px] md:text-[24px] leading-[26px] sm:leading-[30px] lg:max-w-[480px] text-[#F2F2F2]">
+                    <p class="mb-3">
+                        She is an art director and animator with a decade of experience in editorial, documentary, and
+                        advertising.
+                    </p>
+                    <p class="mb-3">
+                        She’s worked for big names like The Atlantic, Adobe, Vox, and Netflix.
+                    </p>
+                    <p class="mb-3">
+                        When she’s not making rad creative work, Yvonne loves to speak and educate on animation and motion
+                        design, and has given talks and run student workshops across the U.S. and internationally.
+                    </p>
                 </div>
                 <div class="mt-[40px]">
-                    <button class="text-base leading-[24px] font-medium rounded-lg border border-[#6AE6D4] text-white px-[20px] py-[12px]">Learn more</button>
+                    <RouterLink to="/contact">
+                        <button
+                            class="text-base leading-[24px] font-medium rounded-lg border border-[#6AE6D4] text-white px-[20px] py-[12px]">Learn
+                            more</button>
+                    </RouterLink>
                 </div>
             </div>
         </div>
